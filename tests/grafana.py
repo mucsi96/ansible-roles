08907808c666c9a9423b3180dcba1e0402349bd8
Igor Bari <mucsi96@gmail.com>
2023-01-27 07:03:04 +0000

    fix grafana authentication + add missing test

--- tests/grafana.py
+++ tests/grafana.py
@@ -2,9 +2,7 @@
 from utils import navigate_and_authenticate, get_browser, wait_for_text, find_element_by_text
 
 with get_browser() as browser:
-    navigate_and_authenticate(browser, '/grafana/d/6be0s85Mk')
-    wait_for_text(browser, 'Grafana Overview')
-    wait_for_text(browser, 'Dashboards')
-    wait_for_text(browser, '200')
-    wait_for_text(browser, '304')
-    wait_for_text(browser, '500')
+    navigate_and_authenticate(browser, '/grafana/admin/users')
+    wait_for_text(browser, 'admin')
+    wait_for_text(browser, 'admin@localhost')
+    wait_for_text(browser, 'Auth Proxy')
